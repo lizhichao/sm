de09774634ff921de1fa206b05c24de7f02f75f2
tanszhe <1018595261@qq.com>
2021-01-29 16:08:29 +0800

    op

--- README.md
+++ README.md
@@ -8,15 +8,18 @@ composer require lizhichao/one-sm
 
 ### 使用
 ```php
+<?php
 require __DIR__ . '/vendor/autoload.php';
 
+$sm3 = new \OneSm\Sm3();
+
 // 字符串签名
-echo \OneSm\Sm3::sign('abc') . PHP_EOL;
-echo \OneSm\Sm3::sign(str_repeat("adfas哈哈哈", 100)) . PHP_EOL;
+echo $sm3->sign('abc') . PHP_EOL;
+echo $sm3->sign(str_repeat("adfas哈哈哈", 100)) . PHP_EOL;
 
 
 // 文件签名
-echo \OneSm\Sm3::signFile(__FILE__) . PHP_EOL;
+echo $sm3->signFile(__FILE__) . PHP_EOL;
 ```
 ### 性能测试
 和 [openssl](https://github.com/openssl/openssl) , [SM3-PHP](https://github.com/DongyunLee/SM3-PHP) 性能测试
@@ -26,12 +29,12 @@ php bench.php
 ```
 结果
 ```
-openssl:c4cae8d8730206d130e1eef9de3e00225da0b556cfcb8d0076561352ff19f769
-one-sm3:c4cae8d8730206d130e1eef9de3e00225da0b556cfcb8d0076561352ff19f769
-SM3-PHP:c4cae8d8730206d130e1eef9de3e00225da0b556cfcb8d0076561352ff19f769
-openssl time:4.8391819000244ms
-one-sm3 time:5.7239532470703ms
-SM3-PHP time:684.2360496521ms
+openssl:4901d7181a1024b8c0f59b8d3c5c6d96b4b707ad10e8ebc8ece5dc49364a3067
+one-sm3:4901d7181a1024b8c0f59b8d3c5c6d96b4b707ad10e8ebc8ece5dc49364a3067
+SM3-PHP:4901d7181a1024b8c0f59b8d3c5c6d96b4b707ad10e8ebc8ece5dc49364a3067
+openssl time:6.3741207122803ms
+one-sm3 time:8.1770420074463ms
+SM3-PHP time:1738.5928630829ms
 
 ```
 [测试代码bench.php](https://github.com/lizhichao/sm/blob/master/bench.php)
--- src/Sm3.php
+++ src/Sm3.php
@@ -4,84 +4,84 @@ namespace OneSm;
 
 class Sm3
 {
-    const IV      = '7380166f4914b2b9172442d7da8a0600a96f30bc163138aae38dee4db0fb0e4e';
-    const LEN     = 512;
-    const STR_LEN = 64;
+    private $IV      = '7380166f4914b2b9172442d7da8a0600a96f30bc163138aae38dee4db0fb0e4e';
+    private $LEN     = 512;
+    private $STR_LEN = 64;
 
-    public static function sign($str)
+    public function sign($str)
     {
         $l   = strlen($str) * 8;
-        $k   = self::getK($l);
-        $bt  = self::getB($k);
+        $k   = $this->getK($l);
+        $bt  = $this->getB($k);
         $str = $str . $bt . pack('J', $l);
 
         $count = strlen($str);
-        $l     = $count / self::STR_LEN;
-        $vr    = hex2bin(self::IV);
+        $l     = $count / $this->STR_LEN;
+        $vr    = hex2bin($this->IV);
         for ($i = 0; $i < $l; $i++) {
-            $vr = self::cf($vr, substr($str, $i * self::STR_LEN, self::STR_LEN));
+            $vr = $this->cf($vr, substr($str, $i * $this->STR_LEN, $this->STR_LEN));
         }
         return bin2hex($vr);
 
     }
 
-    private static function getK($l)
+    private function getK($l)
     {
-        $v = $l % self::LEN;
-        return $v + self::STR_LEN < self::LEN
-            ? self::LEN - self::STR_LEN - $v - 1
-            : (self::LEN * 2) - self::STR_LEN - $v - 1;
+        $v = $l % $this->LEN;
+        return $v + $this->STR_LEN < $this->LEN
+            ? $this->LEN - $this->STR_LEN - $v - 1
+            : ($this->LEN * 2) - $this->STR_LEN - $v - 1;
     }
 
-    private static function getB($k)
+    private function getB($k)
     {
         $arg = [128];
         $arg = array_merge($arg, array_fill(0, intval($k / 8), 0));
         return pack('C*', ...$arg);
     }
 
-    public static function signFile($file)
+    public function signFile($file)
     {
         $l  = filesize($file) * 8;
-        $k  = self::getK($l);
-        $bt = self::getB($k) . pack('J', $l);
+        $k  = $this->getK($l);
+        $bt = $this->getB($k) . pack('J', $l);
 
         $hd  = fopen($file, 'r');
-        $vr  = hex2bin(self::IV);
-        $str = fread($hd, self::STR_LEN);
-        if ($l > self::LEN - self::STR_LEN - 1) {
+        $vr  = hex2bin($this->IV);
+        $str = fread($hd, $this->STR_LEN);
+        if ($l > $this->LEN - $this->STR_LEN - 1) {
             do {
-                $vr  = self::cf($vr, $str);
-                $str = fread($hd, self::STR_LEN);
+                $vr  = $this->cf($vr, $str);
+                $str = fread($hd, $this->STR_LEN);
             } while (!feof($hd));
         }
 
         $str   = $str . $bt;
         $count = strlen($str) * 8;
-        $l     = $count / self::LEN;
+        $l     = $count / $this->LEN;
         for ($i = 0; $i < $l; $i++) {
-            $vr = self::cf($vr, substr($str, $i * self::STR_LEN, self::STR_LEN));
+            $vr = $this->cf($vr, substr($str, $i * $this->STR_LEN, $this->STR_LEN));
         }
         return bin2hex($vr);
     }
 
 
-    private static function t($i)
+    private function t($i)
     {
         return $i < 16 ? 0x79cc4519 : 0x7a879d8a;
     }
 
-    private static function cf($ai, $bi)
+    private function cf($ai, $bi)
     {
         $wr = array_values(unpack('N*', $bi));
         for ($i = 16; $i < 68; $i++) {
-            $wr[$i] = self::p1($wr[$i - 16]
+            $wr[$i] = $this->p1($wr[$i - 16]
                     ^
                     $wr[$i - 9]
                     ^
-                    self::lm($wr[$i - 3], 15))
+                    $this->lm($wr[$i - 3], 15))
                 ^
-                self::lm($wr[$i - 13], 7)
+                $this->lm($wr[$i - 13], 7)
                 ^
                 $wr[$i - 6];
         }
@@ -93,50 +93,50 @@ class Sm3
         list($a, $b, $c, $d, $e, $f, $g, $h) = array_values(unpack('N*', $ai));
 
         for ($i = 0; $i < 64; $i++) {
-            $ss1 = self::lm(
-                (self::lm($a, 12) + $e + self::lm(self::t($i), $i % 32) & 0xffffffff),
+            $ss1 = $this->lm(
+                ($this->lm($a, 12) + $e + $this->lm($this->t($i), $i % 32) & 0xffffffff),
                 7);
-            $ss2 = $ss1 ^ self::lm($a, 12);
-            $tt1 = (self::ff($i, $a, $b, $c) + $d + $ss2 + $wr1[$i]) & 0xffffffff;
-            $tt2 = (self::gg($i, $e, $f, $g) + $h + $ss1 + $wr[$i]) & 0xffffffff;
+            $ss2 = $ss1 ^ $this->lm($a, 12);
+            $tt1 = ($this->ff($i, $a, $b, $c) + $d + $ss2 + $wr1[$i]) & 0xffffffff;
+            $tt2 = ($this->gg($i, $e, $f, $g) + $h + $ss1 + $wr[$i]) & 0xffffffff;
             $d   = $c;
-            $c   = self::lm($b, 9);
+            $c   = $this->lm($b, 9);
             $b   = $a;
             $a   = $tt1;
             $h   = $g;
-            $g   = self::lm($f, 19);
+            $g   = $this->lm($f, 19);
             $f   = $e;
-            $e   = self::p0($tt2);
+            $e   = $this->p0($tt2);
         }
 
         return pack('N*', $a, $b, $c, $d, $e, $f, $g, $h) ^ $ai;
     }
 
 
-    private static function ff($j, $x, $y, $z)
+    private function ff($j, $x, $y, $z)
     {
         return $j < 16 ? $x ^ $y ^ $z : ($x & $y) | ($x & $z) | ($y & $z);
     }
 
-    private static function gg($j, $x, $y, $z)
+    private function gg($j, $x, $y, $z)
     {
         return $j < 16 ? $x ^ $y ^ $z : ($x & $y) | (~$x & $z);
     }
 
 
-    private static function lm($a, $n)
+    private function lm($a, $n)
     {
         return ($a >> (32 - $n) | (($a << $n) & 0xffffffff));
     }
 
-    private static function p0($x)
+    private function p0($x)
     {
-        return $x ^ self::lm($x, 9) ^ self::lm($x, 17);
+        return $x ^ $this->lm($x, 9) ^ $this->lm($x, 17);
     }
 
-    private static function p1($x)
+    private function p1($x)
     {
-        return $x ^ self::lm($x, 15) ^ self::lm($x, 23);
+        return $x ^ $this->lm($x, 15) ^ $this->lm($x, 23);
     }
 
 }
